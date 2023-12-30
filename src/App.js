b62123a7f7934591948ac0203208d4a174f85034
Daniel Ohinoyi <bizwithdan@gmail.com>
2023-12-30 23:37:54 +0100

    coding the sectionWrapper and Button Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import { Download, Features, SectionWrapper } from './components';
+import assets from './assets';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">React Native NFT Showcase</h1>
-    </div>
+    <>
+      <SectionWrapper
+        title="Your own store of Nifty NFTs. Start Selling & Growing"
+        description="Buy, store, collect NFTs, exchange & earn crypto. Join 35+ million people using ProNef Marketplace."
+
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Download from "./Download";
+import Features from "./Features";
+import SectionWrapper from "./SectionWrapper";
+
+
+export {
+    Download, Features, SectionWrapper
+}
