6d53de4bc70f31b7059e6177f40d4cd5da5e522c
Daniel Ohinoyi <bizwithdan@gmail.com>
2024-01-01 13:24:41 +0100

    finished building the landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Download, Features, SectionWrapper } from './components';
 import assets from './assets';
+import styles from './styles/Global';
 
 const App = () => {
   return (
@@ -7,19 +8,41 @@ const App = () => {
       <SectionWrapper
         title="Your own store of Nifty NFTs. Start Selling & Growing"
         description="Buy, store, collect NFTs, exchange & earn crypto. Join 35+ million people using ProNef Marketplace."
-
         showBtn
         mockupImg={assets.homeHero}
         banner="banner"
       />
+
       <SectionWrapper
         title="Smart User Interface Marketplace"
         description="Experience a cosmic UI of ProNef NFT Marketplace. Smooth constant colours of a fluent UI design."
-
         mockupImg={assets.homeCards}
         reverse
       />
+
       <Features />
+
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo, which runs natively on all user's devices. You can easily get your app into people's hands."
+        mockupImg={assets.feature}
+        reverse
+      />
+
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs, while the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+
+      <Download />
+
+      <div className='px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04'>
+        <p className={`${styles.pText} ${styles.whiteText}`}>Built with love and skillz by {" "}
+          <span className='bold'>Daniel Ohinoyi Isah</span>
+        </p>
+      </div>
     </>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,12 +8,7 @@ const Button = ({ assetUrl, link }) => {
       className={styles.btnBlack}
       onClick={() => window.open(link, "_blank")}
     >
-      <img
-        src={assetUrl}
-        alt="expo_icon"
-        className={styles.btnIcon}
-        style={{ marginRight: 10 }}
-      />
+      <img src={assetUrl} alt="expo_icon" className={styles.btnIcon} />
       <div className="flex flex-col justify-start ml-4">
         <p
           className={`${styles.btnText}
